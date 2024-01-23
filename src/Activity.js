6f258796252a7ca64ac8450f3c6bd2672176e3bb
vasumanoharan <vasumanoharan98@gmail.com>
2024-01-23 10:59:02 +0530

    second

--- src/Activity.js
+++ src/Activity.js
@@ -1,13 +1,16 @@
 import { useState } from "react"
 function Activity() {
-    var activity = [
-        {
-            id: 1,
-            work: "Go for a walk"
-        }
-    ]
-
+    // var activity = [
+    //     {
+    //         id: 1,
+    //         work: "Go for a walk"
+    //     }
+    // ]
+    
+    // value input using usestate
     var [fruitInput, setfruitInput] = useState('')
+
+    // Array input
     var [fruits, setfruits] = useState([
         {
             id: 1,
@@ -23,8 +26,10 @@ function Activity() {
         // },
     ])
 
+    // delete button created pass deletedID as ID
     var deleteBtn = (deleteId) => {
-        var updateFruit = fruits.filter(
+        // filter used if false wont return, true it return
+        var updateFruit = fruits.filter(        
             (fruit) => {
                 if (deleteId == fruit.id) {
                     return false
@@ -34,10 +39,11 @@ function Activity() {
                 }
             }
         )
-
+            // finally update the deleted one (taken from usestate)
         setfruits(updateFruit)
     }
 
+    // Adding fruits (...) ID and Work is input is added
     const addFruit = () => {
         setfruits([...fruits, {
             id: (fruits.length) + 1,
@@ -51,29 +57,33 @@ function Activity() {
             <div className="bg-[#BDB4EA] grow rounded-lg p-10">
                 <h1 className="text-4xl font-bold">Todays Activity</h1>
                 {
+                    // mapping is used for print and loop for showing
                     fruits.map(
                         (data) => {
                             return (
                                 <div>
                                     <span>{data.id}.{data.work}</span>
+                                    <button style={{ cursor: "pointer",backgroundColor:'black',color:'white',padding:'4px',marginLeft:'10px',marginTop:'10px',borderRadius:'5px'}} onClick={() => deleteBtn(data.id)}>delete</button>
                                 </div>
                             )
                         }
                     )
                 }
             </div>
+
+
             <div className="p-6 bg-red-500 rounded-lg">
 
                 <h1 className="text-2xl font-medium">To do List</h1>
                 <input type="text" placeholder="Enter the Value" className="border-solid	border-2 border-red-500" value={fruitInput} onChange={(evnt) => setfruitInput(evnt.target.value)}></input>
-                <button className="border-solid border-2  border-red-500  rounded-md" onClick={addFruit}>Add Data <i className="icon-address-book-lite"></i></button>
+                <button className="border-solid border-2  border-black-500  rounded-md bg-black text-white p-[2px] ml-2 " onClick={addFruit}>Add Data <i className="icon-address-book-lite"></i></button>
 
                 {
                     fruits.map((fruit, index) => {
                         return (
                             <div key={index}>
                                 <span>{fruit.work}</span>
-                                <button style={{ cursor: "pointer" }} onClick={() => deleteBtn(fruit.id)}>delete</button>
+                                <button style={{ cursor: "pointer",backgroundColor:'black',color:'white',padding:'4px',marginLeft:'10px',marginTop:'10px',borderRadius:'5px'}} onClick={() => deleteBtn(fruit.id)}>delete</button>
 
                             </div>
                         )
